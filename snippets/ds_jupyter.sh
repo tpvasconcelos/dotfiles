c8345d49d2337e057721c619a347c8e14e1b2281
Tomas Pereira de Vasconcelos <tomas@tiqets.com>
2020-11-04 18:03:02 +0100

    add snippet for poetry

--- snippets/ds_jupyter.sh
+++ snippets/ds_jupyter.sh
@@ -1,3 +1,5 @@
+# pipenv  ---
+
 pipenv install ipython ipykernel jupyter jupyterlab --skip-lock
 python -m ipykernel install --user --name="$(basename "$VIRTUAL_ENV")"
 # OR: python -m ipython kernel install --user --name="$(basename "$VIRTUAL_ENV")"
@@ -8,3 +10,17 @@ pipenv install black isort rpy2 blackcellmagic jupyterlab_code_formatter ipywidg
 pipenv run jupyter labextension install @jupyterlab/toc @ryantam626/jupyterlab_code_formatter @jupyter-widgets/jupyterlab-manager@2.0 @oriolmirosa/jupyterlab_materialdarker jupyterlab-plotly @jupyter-widgets/jupyterlab-manager plotlywidget@4.9.0
 jupyter serverextension enable --py jupyterlab_code_formatter
 pipenv run jupyter lab build
+
+
+# poetry  ---
+
+poetry add ipython ipykernel jupyter jupyterlab
+poetry run python -m ipykernel install --user --name="$(basename "$VIRTUAL_ENV")"
+# OR: poetry run python -m ipython kernel install --user --name="$(basename "$VIRTUAL_ENV")"
+
+# Extensions
+poetry run jupyter lab build
+poetry add black isort rpy2 blackcellmagic jupyterlab_code_formatter ipywidgets jupyter-dash
+poetry run jupyter labextension install @jupyterlab/toc @ryantam626/jupyterlab_code_formatter @jupyter-widgets/jupyterlab-manager@2.0 @oriolmirosa/jupyterlab_materialdarker jupyterlab-plotly @jupyter-widgets/jupyterlab-manager plotlywidget@4.9.0
+poetry run jupyter serverextension enable --py jupyterlab_code_formatter
+poetry run jupyter lab build
