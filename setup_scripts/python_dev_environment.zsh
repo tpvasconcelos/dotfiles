4227388643d3d9ab48d6ab1d5bffb8d2db9952ce
Tomas Pereira de Vasconcelos <tomas@tiqets.com>
2020-11-30 01:36:33 +0100

    fix python/tau

--- setup_scripts/python_dev_environment.zsh
+++ setup_scripts/python_dev_environment.zsh
@@ -1,21 +1,24 @@
 #!/usr/bin/env zsh
+set -eu
 
-echo "🚀 Installing pyenv..."
+log_info "Installing pyenv..."
 brew install pyenv
 
-source_if_exists "${DOTFILES}/shell/functions/interactive/python"
-python_upgrade_all
+load "${SHELL_DIR_INTERACTIVE}/functions/string"
+load "${SHELL_DIR_INTERACTIVE}/functions/tau"
+tau_install_all
+# FIXME: tau_install_all && tau_cleanup
 
-echo "🚀 Installing pipenv..."
+log_info "Installing pipenv..."
 brew install pipenv
 
-echo "🚀 Installing brew's python (and unlinking it!)..."
+log_info "Installing brew's python (and unlinking it!)..."
 # TODO: is this step necessary? isnt python already installed? maybe simply unlink?
 brew install python && brew unlink python
 
-echo "🚀 Installing poetry..."
+log_info "Installing poetry..."
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
 poetry completions zsh > "$BREW_PREFIX/share/zsh/site-functions/_poetry"
 
-echo "🚀 update, upgrade, and cleanup..."
+log_info "update, upgrade, and cleanup..."
 brew update && brew upgrade && brew cleanup
