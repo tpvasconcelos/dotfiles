db2bb5edc081473854fa4ec29e1106304602142a
Tomas Pereira de Vasconcelos <tomas@tiqets.com>
2020-12-07 00:09:03 +0100

    shallow clones

--- setup_scripts/setup_shells.sh
+++ setup_scripts/setup_shells.sh
@@ -15,10 +15,10 @@ log_info "Installing Powerlevel10k..."
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
 log_info "Installing zsh-autosuggestions..."
-git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 
 log_info "Installing zsh-syntax-highlighting..."
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 
 PATH_TO_SHELL="$(brew --prefix)/bin/zsh"
 log_info "Changing the default shell to the brew-installed zsh shell ---> ${PATH_TO_SHELL}"
