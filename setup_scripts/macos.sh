b25f16611f846c32acc5eb1954a51b8e8dbc7af8
Tomas Pereira de Vasconcelos <tomas@tiqets.com>
2020-12-21 12:56:42 +0000

    remove deprecated macos setting

--- setup_scripts/macos.sh
+++ setup_scripts/macos.sh
@@ -416,9 +416,6 @@ defaults write com.apple.dock expose-group-by-app -bool false
 # Disable Dashboard
 defaults write com.apple.dashboard mcx-disabled -bool true
 
-# Don’t show Dashboard as a Space
-defaults write com.apple.dock dashboard-in-overlay -bool true
-
 # Don’t automatically rearrange Spaces based on most recent use
 defaults write com.apple.dock mru-spaces -bool false
 
