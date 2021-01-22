d8e809f215b33b4112892783ffd27145de4b1fdb
Tomas Pereira de Vasconcelos <tomas@tiqets.com>
2021-01-23 00:23:00 +0100

    edit ds_jupyter.sh

--- snippets/ds_jupyter.sh
+++ snippets/ds_jupyter.sh
@@ -6,21 +6,26 @@ python -m ipykernel install --user --name="$(basename "$VIRTUAL_ENV")"
 
 # Extensions
 pipenv run jupyter lab build
-pipenv install black isort rpy2 blackcellmagic jupyterlab_code_formatter ipywidgets jupyter-dash --skip-lock
-pipenv run jupyter labextension install @jupyterlab/toc @ryantam626/jupyterlab_code_formatter @jupyter-widgets/jupyterlab-manager@2.0 @oriolmirosa/jupyterlab_materialdarker jupyterlab-plotly @jupyter-widgets/jupyterlab-manager plotlywidget@4.9.0
-jupyter serverextension enable --py jupyterlab_code_formatter
+pipenv install black isort blackcellmagic "ipywidgets>=7.5" jupyter-dash --skip-lock
+pipenv run jupyter labextension install @jupyterlab/toc \
+                                        @jupyter-widgets/jupyterlab-manager \
+                                        jupyterlab-plotly \
+                                        plotlywidget
 pipenv run jupyter lab build
 
 
 # poetry  ---
 
-poetry add ipython ipykernel jupyter jupyterlab
-poetry run python -m ipykernel install --user --name="$(basename "$VIRTUAL_ENV")"
+poetry add -D ipython ipykernel jupyter jupyterlab
+env_name="$(basename "$(poetry env info -p)")"
+poetry run python -m ipykernel install --user --name="${env_name}"
 # OR: poetry run python -m ipython kernel install --user --name="$(basename "$VIRTUAL_ENV")"
 
 # Extensions
 poetry run jupyter lab build
-poetry add black isort rpy2 blackcellmagic jupyterlab_code_formatter ipywidgets jupyter-dash
-poetry run jupyter labextension install @jupyterlab/toc @ryantam626/jupyterlab_code_formatter @jupyter-widgets/jupyterlab-manager@2.0 @oriolmirosa/jupyterlab_materialdarker jupyterlab-plotly @jupyter-widgets/jupyterlab-manager plotlywidget@4.9.0
-poetry run jupyter serverextension enable --py jupyterlab_code_formatter
+poetry add -D black isort blackcellmagic "ipywidgets>=7.5" jupyter-dash
+poetry run jupyter labextension install @jupyterlab/toc \
+                                        @jupyter-widgets/jupyterlab-manager \
+                                        jupyterlab-plotly \
+                                        plotlywidget
 poetry run jupyter lab build
