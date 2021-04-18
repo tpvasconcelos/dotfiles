aa6dc45afeb8ffb2bdefaedc64f483f21bd930ad
Tomas Pereira de Vasconcelos <tomasvasconcelos1@gmail.com>
2021-04-19 00:19:35 +0200

    replace log_info with echo

--- setup_scripts/setup_shells.sh
+++ setup_scripts/setup_shells.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
 set -eu
 
-log_info "Installing bash..."
+echo "Installing bash..."
 brew install bash
 brew install bash-completion2
 
-log_info "Installing zsh..."
+echo "Installing zsh..."
 brew install zsh
 
-log_info "Installing oh-my-zsh..."
+echo "Installing oh-my-zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-log_info "Installing Powerlevel10k..."
+echo "Installing Powerlevel10k..."
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
-log_info "Installing zsh-autosuggestions..."
+echo "Installing zsh-autosuggestions..."
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 
-log_info "Installing zsh-syntax-highlighting..."
+echo "Installing zsh-syntax-highlighting..."
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 
 PATH_TO_SHELL="$(brew --prefix)/bin/zsh"
-log_info "Changing the default shell to the brew-installed zsh shell ---> ${PATH_TO_SHELL}"
+echo "Changing the default shell to the brew-installed zsh shell ---> ${PATH_TO_SHELL}"
 if ! fgrep -q "${PATH_TO_SHELL}" /etc/shells; then
   echo "${PATH_TO_SHELL}" | sudo tee -a /etc/shells
   chsh -s "${PATH_TO_SHELL}"
