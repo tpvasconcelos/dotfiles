6083a1a0d956b69ea39cc0e1b04f805fb75fff31
Tomas Pereira de Vasconcelos <tomasvasconcelos1@gmail.com>
2021-04-23 20:03:05 +0100

    unlink homebrew's python

--- setup_scripts/python_dev_environment.zsh
+++ setup_scripts/python_dev_environment.zsh
@@ -1,6 +1,9 @@
 #!/usr/bin/env zsh
 set -eu
 
+log_info "Unlinking Homebrew's python..."
+brew unlink python
+
 log_info "Installing pyenv..."
 brew install pyenv
 
@@ -11,13 +14,6 @@ tau_install_all
 
 log_info "Installing pipenv..."
 brew install pipenv
-
-log_info "Installing brew's python (and unlinking it!)..."
-# TODO: is this step necessary?
-#       isn't brew's python already installed?
-#       maybe simply unlink?
-#       or simply do nothing?
-#brew install python && brew unlink python
 
 log_info "Installing poetry..."
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
