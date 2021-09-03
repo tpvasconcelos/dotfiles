08759c992df6640a87825685f17afdc2912eae0e
Tomas Pereira de Vasconcelos <tomasvasconcelos1@gmail.com>
2021-09-03 17:38:53 +0200

    update fzf helper functions

--- shell/functions/fzf.zsh
+++ shell/functions/fzf.zsh
@@ -8,6 +8,17 @@ fh() {
   print -z "$(history | fzf +s --tac | sed -E 's/ *[0-9]*\*? *//' | sed -E 's/\\/\\\\/g')"
 }
 
+# autojump
+j() {
+  local target
+  if [[ "$#" -ne 0 ]]; then
+    target="$(autojump "$@")"
+  else
+    target="$(autojump -s | sort -k1gr | awk '$1 ~ /[0-9]:/ && $2 ~ /^\// { for (i=2; i<=NF; i++) { print $(i) } }' |  fzf --height 40% --reverse --inline-info)"
+  fi
+  cd "${target}" || return
+}
+
 
 # fkill - kill processes - list only the ones you can kill. Modified the earlier script.
 fkill() {
@@ -22,23 +33,20 @@ fkill() {
   fi
 }
 
-
-# fbr - checkout git branch (including remote branches), sorted by most recent commit, limit 30 last branches
-fbr() {
-  local branches branch
-  branches=$(git for-each-ref --no-merged master --count=30 --sort=-committerdate --format='%(committerdate:relative),%(authorname),%(refname:short),%(if)%(upstream)%(then)%(upstream)%(end)' refs/heads |
-             column -t -s ',' | grep -v "$(git branch --show-current)") &&
-  branch=$(echo "$branches" |
-           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
-  git checkout "$(echo "$branch" | sed "s/.* //" | sed "s#origin/*/##")"
-  # sed "s#remotes/[^/]i*/##"
+_get_branch() {
+  git --no-pager branch \
+      --sort=-committerdate \
+      --format="%(if)%(HEAD)%(then)%(else)%(if:equals=HEAD)%(refname:strip=3)%(then)%(else)%(refname:short)%(end)%(end)" \
+    | sed '/^$/d' \
+    | fzf --no-multi -n 2 --ansi --preview="git --no-pager log -50 --pretty='format:%C(auto)%h [%ad] %s' --date=short '..{1}'"
 }
 
-
-# fdb( - delete git branch (including remote branches)
+# fdb - delete git branch
 fdb() {
-  local branches branch
-  branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
-    branch=$(echo "$branches" | fzf --multi) &&
-    git branch -D "$(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")"
+  _get_branch | xargs -I {} git branch -D {}
+}
+
+# fco - checkout git branch
+fco() {
+  _get_branch | xargs -I {} git checkout {}
 }
