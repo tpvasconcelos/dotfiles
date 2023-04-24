34d3b9deca6bbff32c3e3fd88edcf849945d40ca
Tomas Pereira de Vasconcelos <tomasvasconcelos1@gmail.com>
2023-04-24 21:41:36 +0200

    update usage of `PYENV_VERSIONS` in bootstrap.zsh

--- bootstrap.zsh
+++ bootstrap.zsh
@@ -126,11 +126,22 @@ fi
 # Python
 ########################
 
-log_info "Installing the following python versions from pyenv: $PYENV_TARGET_VERSIONS"
-tau-install-all
-# TODO: tau-cleanup or something similar to automatically
-#       add all minor versions as global versions
-tau-global 3.10
+if [[ -n ${PYENV_VERSIONS+x} ]]; then
+  # If the PYENV_VERSIONS environment variable is set, parse the
+  # space-separated list into an array and store it in $py_versions.
+  # For instance, "2.7 3.6 3.8.5" becomes ("2.7" "3.6" "3.8.5")
+  IFS=" " read -rA py_versions <<<"${PYENV_VERSIONS}"
+else
+  # else... default to the following versions
+  py_versions=("3.7" "3.8" "3.9" "3.10" "3.11" "3.12")
+fi
+
+log_info "Installing the following python versions with pyenv: $py_versions"
+for py_version in "${py_versions[@]}"; do
+  tau-install "$py_version"
+  tau-global "$py_version"
+done
+
 eval "$(pyenv init --path)"
 
 POETRY_OMZ_PLUGIN_PATH="$ZSH_CUSTOM/plugins/poetry"
--- shell/functions/tau.zsh
+++ shell/functions/tau.zsh
@@ -29,9 +29,10 @@ tau-install() {
   #
   #   $ tau-install 3.7.1
   #   [ℹ] Installing Python 3.7.1
+  #   ...
   #
   #   $ tau-install 3.10-dev
-  #   [✘] The input '3.10-dev' doesnt match the a valid version number.
+  #   [✘] The input '3.10-dev' does not match a valid version number...
   #
   local py_version_user_input="${1}"
   local py_version_patch
@@ -49,7 +50,7 @@ tau-install() {
     # Alternatively, you can pass an empty string, in which case, the latest available
     # stable version will be pulled and installed.
     # e.g. --> "2" or "3.6" or "3.8.5" or "" (empty string)
-    log_error "The input '${py_version_user_input}' doesnt match the a valid version number. Please pass a valid version number in the format of 'major', 'major.minor', or 'major.minor.patch', or leave the input empty to install the latest stable version."
+    log_error "The input '${py_version_user_input}' does not match a valid version number. Please pass a valid version number in the format of 'major', 'major.minor', or 'major.minor.patch', or leave the input empty to install the latest stable version."
     return 1
   elif [[ "${py_version_user_input}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     # If the input matches an exact patch version (e.g. "3.8.5") we'll try to
@@ -76,7 +77,7 @@ tau-install() {
       return 1
     elif [[ ! "${py_version_patch}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
       # Something else went wrong...
-      log_error "Something went wrong parsing the python version. '${py_version_patch}' doesnt match the right regex."
+      log_error "Something went wrong parsing the python version. '${py_version_patch}' does not match the right regex."
       return 1
     fi
   fi
@@ -96,45 +97,6 @@ tau-install() {
     log_info "Installing Python '${py_version_patch}'"
     pyenv install "${py_version_patch}"
   fi
-}
-
-tau-install-all() {
-  # Run `tau-install` for all Python versions in the
-  # $PYENV_TARGET_VERSIONS array.
-  #
-  # If $$PYENV_TARGET_VERSIONS_OVERWRITE is set, it will take
-  # precedence over $PYENV_TARGET_VERSIONS.
-  #
-  # Arguments:
-  #   * <NONE>
-  #
-  # Examples:
-  #
-  #   $ tau-install-all
-  #   [⋯] Collected python versions: (3.7 3.8 3.9 3.10 3.11 3.12)
-  #   [⚠] Skipping: Python 3.7.10 is already installed.
-  #   ...
-  #   [ℹ] Installing Python 3.12.0
-  #
-  #   $ PYENV_TARGET_VERSIONS_OVERWRITE="3.7 3.8.5" tau-install-all
-  #   [⋯] Collected python versions: (3.7 3.8.5)
-  #   [⚠] Skipping: Python 3.7.10 is already installed.
-  #   [ℹ] Installing Python 3.8.5
-  #
-  local py_versions pyv
-  if [[ -n ${PYENV_TARGET_VERSIONS_OVERWRITE+x} ]]; then
-    # If the $PYENV_TARGET_VERSIONS_OVERWRITE environment variable is set,
-    # parse the space-separated list into an array and store it in $py_versions
-    # For instance, "2.7 3.6 3.8.5" becomes ("2.7" "3.6" "3.8.5")
-    IFS=" " read -rA py_versions <<<"${PYENV_TARGET_VERSIONS_OVERWRITE}"
-  else
-    # else... simply fallback to the default versions in $PYENV_TARGET_VERSIONS
-    py_versions=("${PYENV_TARGET_VERSIONS[@]}")
-  fi
-  log_debug "Collected python versions: (${py_versions[*]})"
-  for pyv in "${py_versions[@]}"; do
-    tau-install "${pyv}"
-  done
 }
 
 tau-clean-pip() {
@@ -219,7 +181,7 @@ tau-global() {
   #   [ℹ] Setting Python 3.9.13 as a global version
   #
   #   $ tau-global 1
-  #   [✘] The input '1' doesnt match the a valid version number.
+  #   [✘] The input '1' does not match a valid version number.
   #
   #   $ tau-global 4.2
   #   [✘] Could not find a match for '4.2'. Are you sure this Python version exists?
@@ -229,7 +191,7 @@ tau-global() {
   local py_version_patch
 
   if [[ ! "${py_version_user_input}" =~ ^[0-9]+\.[0-9]+$ ]]; then
-    log_error "The input '${py_version_user_input}' doesnt match the a valid version number."
+    log_error "The input '${py_version_user_input}' does not match a valid version number."
     return 1
   fi
   py_version_patch="$(pyenv versions | ggrep -Po "(?<= )[0-9]+\.[0-9]+\.[0-9]+" | grep "^${py_version_user_input}" | tail -n 1 | xargs)"
@@ -242,7 +204,7 @@ tau-global() {
     return 1
   elif [[ ! "${py_version_patch}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     # Something else went wrong...
-    log_error "Something went wrong parsing the python version. '${py_version_patch}' doesnt match the right regex."
+    log_error "Something went wrong parsing the python version. '${py_version_patch}' does not match the right regex."
     return 1
   fi
   log_info "Setting Python ${py_version_patch} as a global version"
