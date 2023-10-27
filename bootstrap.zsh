0712d0e6ccd8b3ecae4dd4a6fbeeeebb38e989d0
Tomas Pereira de Vasconcelos <tomasvasconcelos1@gmail.com>
2023-10-27 09:01:27 +0200

    Update pipx zsh plugin

--- bootstrap.zsh
+++ bootstrap.zsh
@@ -113,8 +113,8 @@ gsc https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my
 log_info "Installing zsh-syntax-highlighting..."
 gsc https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 
-log_info "Installing zsh-plugin-pipx..."
-gsc https://github.com/omBratteng/zsh-plugin-pipx.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-plugin-pipx"
+log_info "Installing thuandt/zsh-pipx..."
+gsc https://github.com/thuandt/zsh-pipx.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-pipx"
 
 if [[ -r "${HOME}/.iterm2_shell_integration.zsh" ]]; then
   log_success "Shell Integration and iTerm2 utilities already installed!"
