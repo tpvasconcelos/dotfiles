d87d9f97087a7bab93cbbdd505bd5112bc5c8245
Tomas Pereira de Vasconcelos <tomasvasconcelos1@gmail.com>
2024-02-15 13:26:13 +0100

    add `largest-files`

--- shell/functions/pathlib.zsh
+++ shell/functions/pathlib.zsh
@@ -1,5 +1,6 @@
 abspath() {
   # Converts any path to its absolute path
+  #
   # Arguments:
   #   * $@ : path-like string
   echo "$(cd "$(dirname "${@}")" && pwd)/$(basename "${@}")"
@@ -8,19 +9,27 @@ abspath() {
 get_stem() {
   # Gets the stem of a given path. Note that this function does *not* check if
   # the path actually exists!
+  #
   # Arguments:
   #   * $1 : path-like string
+  #
   # Examples:
+  #
   #   $ get_stem /path/to/script.sh
   #   script
+  #
   #   $ get_stem /path/to/script.min.js
   #   script.min
+  #
   #   $ get_stem /path/to/file
   #   file
+  #
   #   $ get_stem /path/to/directory
   #   directory
+  #
   #   $ get_stem meh
   #   meh
+  #
   #   $ get_stem
   #   ✖ get_stem: No path provided!
   if [[ -z "${*}" ]]; then
@@ -34,4 +43,26 @@ get_stem() {
 
 sizeof() {
  du -hs "${*:-.}"
+}
+
+largest-files() {
+  # Print the largest files in a directory
+  #
+  # Arguments:
+  #   * $1 : directory (defaults to current directory)
+  #   * $2 : number of files to show (defaults to 10)
+  #
+  # Examples:
+  #
+  #   $ largest-files . 3
+  #   1.5G    ./path/to/bigfile
+  #   1.2G    ./other/path/to/bigfile
+  #   1.1G    ./other/path/to/another/bigfile
+  #
+  #   $ largest-files ./other 2
+  #   1.2G    ./other/path/to/bigfile
+  #   1.1G    ./other/path/to/another/bigfile
+  local dir="${1:-.}"
+  local num="${2:-10}"
+  find "${dir}" -type f -exec du -h {} + | sort -rh | head -n "${num}"
 }
