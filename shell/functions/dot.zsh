287eec9df458d50c4329375e8144d98ab74a089b
Tomas Pereira de Vasconcelos <tomasvasconcelos1@gmail.com>
2025-10-18 22:27:14 +1100

    Update `dot.zsh`

--- shell/functions/dot.zsh
+++ shell/functions/dot.zsh
@@ -1,15 +1,13 @@
 dot-backup() {
   # Copy files from the local machine to Mackup's backup directory
-  log_info "Backing up Mackup-managed files..."
-  mackup backup --force
   # https://github.com/lra/mackup/issues/1924
-  mackup uninstall --force
+  log_info "Backing up Mackup-managed files..."
+  mackup backup --force && mackup install --force
 }
 
 dot-update-system() {
   # Copy files from Mackup's backup directory to the local machine
-  log_info "Restoring Mackup-managed files..."
-  mackup restore --force
   # https://github.com/lra/mackup/issues/1924
-  mackup uninstall --force
+  log_info "Restoring Mackup-managed files..."
+  mackup restore --force && mackup uninstall --force
 }
